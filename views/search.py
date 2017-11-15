657c82aee0bd254cd214fc82d70eca6ab753b4c6
jhsm croonen <info@jcroonen.nl>
2017-11-15 06:02:12 +0100

    search page integrated with results

--- views/search.py
+++ views/search.py
@@ -6,20 +6,19 @@ from flac.db import get_albums_by_title, get_general_search
 from flac.forms import SearchForm
 
 
-def searchq(request, query):
+def searchresponse(context, request):
     template = loader.get_template('flac/searchq.html')
-    return HttpResponse(template.render(
-        {
+    return HttpResponse(template.render(context, request))
+
+
+def searchq(request, query):
+    context = {
             'query': query,
             'albums': get_albums_by_title(query)
-        }, request))
+        }
+    return searchresponse(context, request)
 
 
 def search(request):
-    # template = loader.get_template('flac/search.html')
-    # return HttpResponse(template.render(
-    #     {
-    #     }, request))
-    form = SearchForm
-    return render(request, 'flac/search.html', {'form': form})
+    return searchresponse({}, request)
 
